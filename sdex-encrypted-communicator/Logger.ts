d9363d7265273f68b4779d17f8290c5c3164b9c1
Jan Wadolowski <janwadolowski@gmail.com>
2023-04-22 01:56:46 +0200

    feat: new logger with configuration

---
+++ sdex-encrypted-communicator/Logger.ts
@@ -0,0 +1,18 @@
+import * as FileSystem from "expo-file-system";
+import { logger as _logger } from "react-native-logs";
+
+const config = {
+  levels: {
+    debug: 0,
+    info: 1,
+    warn: 2,
+    error: 3,
+  },
+  transportOptions: {
+    FS: FileSystem,
+    fileName: "sdex-communicator-logs.txt",
+  },
+};
+
+let logger = _logger.createLogger<"debug" | "info" | "warn" | "error">(config);
+export default logger;
