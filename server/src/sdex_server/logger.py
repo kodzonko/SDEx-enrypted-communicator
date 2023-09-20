811a55c6576f146cd17ae13e57324b6ecae2688f
Jan Wadolowski <janwadolowski@gmail.com>
2023-09-20 04:01:01 +0200

    style(server): reformat docstring, remove examples

--- server/src/sdex_server/logger.py
+++ server/src/sdex_server/logger.py
@@ -10,6 +10,7 @@ from loguru._defaults import LOGURU_FORMAT
 class InterceptHandler(logging.Handler):
     """
     Default handler from examples in loguru documentation.
+
     See https://loguru.readthedocs.io/en/stable/overview.html#entirely-compatible-with-standard-logging
     """
 
@@ -35,24 +36,17 @@ def format_record(record: dict) -> str:
     """
     Custom format for loguru loggers.
     Uses pformat for log any data like request/response body during debug.
-    Works with logging if loguru handler it.
-    Example:
-    >>> payload = [{"users":[{"name": "Nick", "age": 87, "is_active": True}, {"name": "Alex", "age": 27, "is_active": True}], "count": 2}]
-    >>> logger.bind(payload=).debug("users payload")
-    >>> [   {   'count': 2,
-    >>>         'users': [   {'age': 87, 'is_active': True, 'name': 'Nick'},
-    >>>                      {'age': 27, 'is_active': True, 'name': 'Alex'}]}]
+    Works with logging if loguru handles it.
     """
-
     format_string = LOGURU_FORMAT
     if record["extra"].get("payload") is not None:
         record["extra"]["payload"] = pformat(
             record["extra"]["payload"], indent=4, compact=True, width=88
         )
-        format_string += "\n<level>{extra[payload]}</level>"
+        format_string += "\n<level>{extra[payload]}</level>"  # type: ignore
 
-    format_string += "{exception}\n"
-    return format_string
+    format_string += "{exception}\n"  # type: ignore
+    return format_string  # type: ignore
 
 
 def init_logging():
@@ -62,19 +56,7 @@ def init_logging():
     WARNING!
     if you call the init_logging in startup event function,
     then the first logs before the application start will be in the old format
-    >>> app.add_event_handler("startup", init_logging)
-    stdout:
-    INFO:     Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
-    INFO:     Started reloader process [11528] using statreload
-    INFO:     Started server process [6036]
-    INFO:     Waiting for application startup.
-    2020-07-25 02:19:21.357 | INFO     | uvicorn.lifespan.on:startup:34 - Application startup complete.
-
     """
-
-    # disable handlers for specific uvicorn loggers
-    # to redirect their output to the default uvicorn logger
-    # works with uvicorn==0.11.6
     loggers = (
         logging.getLogger(name)
         for name in logging.root.manager.loggerDict
